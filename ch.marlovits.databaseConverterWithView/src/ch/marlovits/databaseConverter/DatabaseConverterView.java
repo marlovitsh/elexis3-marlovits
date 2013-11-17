065c6ad3382a8e60d0e943a51362b320b17230d0
marlovitsh <marlovitsh@gmail.com>
2013-11-17 20:17:31 +0100

    save to github

---
+++ ch.marlovits.databaseConverterWithView/src/ch/marlovits/databaseConverter/DatabaseConverterView.java
@@ -0,0 +1,43 @@
+package ch.marlovits.databaseConverter;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.part.ViewPart;
+
+public class DatabaseConverterView extends ViewPart {
+	public static final String ID = "ch.marlovits.databaseconverter.views.MarlovitsDataBaseConverterView";
+
+	public DatabaseConverterView() {
+	}
+
+	public void createPartControl(Composite parent) {
+		Composite comp = new Composite(parent, SWT.NONE);
+		GridLayout gl = new GridLayout(2, false);
+		comp.setLayout(gl);
+
+		Label lbl = new Label(comp, SWT.None);
+		lbl.setText("Konvertieren aller MFULists von alter zu neuer Version");
+
+		Button btnConvertMFULists = new Button(comp, SWT.PUSH);
+		btnConvertMFULists.setText("Convert MFULists");
+		btnConvertMFULists.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DatabaseConverters.convertAllMFUEntries();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
+	}
+
+	@Override
+	public void setFocus() {
+	}
+}
---
+++ ch.marlovits.databaseConverterWithView/src/ch/marlovits/databaseConverter/DatabaseConverters.java
@@ -0,0 +1,86 @@
+package ch.marlovits.databaseConverter;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import ch.elexis.core.data.Anwender;
+import ch.elexis.core.data.PersistentObject;
+import ch.elexis.core.data.Query;
+
+/**
+ * TO DO BEFORE CONVERSION: temporarily add MFUList.java to ch.rgw.tools in
+ * ch.rgw.utility. After the conversion simply delete both modifications again!
+ * 
+ * @author marlovitsh
+ * 
+ */
+
+public class DatabaseConverters {
+	// *******************************************************************************************
+	// *** convert extinfo entries from old style to new style:
+	// *** ch.rgw.tools.MFUList -> ch.elexis.core.data.util.MFUList
+	// *******************************************************************************************
+
+	/**
+	 * convert all extinfos of all users/Anwender. Calls convertKontaktMFU().
+	 */
+	public static void convertAllMFUEntries() {
+		Query<Anwender> qbe = new Query<Anwender>(Anwender.class);
+		qbe.clear(true); // *** include deleted entries, too
+		List<Anwender> userList = qbe.execute();
+		if ((userList == null) || (userList.size() < 1))
+			return;
+		for (Anwender user : userList) {
+			convertKontaktMFU(user);
+		}
+	}
+
+	/**
+	 * converts the MFUList entry "LeistungenMFU" from old version
+	 * (ch.rgw.tools.MFUList) to new version (ch.elexis.core.data.util.MFUList)
+	 * for the Anwender user
+	 * 
+	 * @param user
+	 *            Anwender, user for which the extinfo is to be corrected
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void convertKontaktMFU(Anwender user) {
+		String MFUName = "LeistungenMFU";
+
+		// *** read extinfo
+		Map extInfoAsMap = user.getMap(PersistentObject.FLD_EXTINFO);
+
+		try {
+			/**
+			 * try to read LeistungenMFU as a Map. read as old version
+			 * (ch.rgw.tools.MFUList). If there is an old version then the entry
+			 * is read correctly. If there is already the new version then the
+			 * conversion fails (cast error) -> catch
+			 */
+			// *** read MFU from extinfo (old version) = SOURCE
+			ch.rgw.tools.MFUList_<String> leistungenMFU = (ch.rgw.tools.MFUList_<String>) extInfoAsMap
+					.get(MFUName);
+			// *** create an empty MFUList (new version) = DEST
+			ch.elexis.core.data.util.MFUList<String> leistungenMFU_NEW = new ch.elexis.core.data.util.MFUList(
+					5, 15);
+			// *** loop through the items of the source MFUList
+			Iterator<String> oldIterator = leistungenMFU.iterator();
+			while (oldIterator.hasNext()) {
+				String value = oldIterator.next();
+				// *** add item to the new version dest MFUList
+				leistungenMFU_NEW.count(value);
+			}
+			// *** delete old MFUList version from extinfo
+			extInfoAsMap.remove(MFUName);
+
+			// *** write new MFUList version to extinfo
+			extInfoAsMap.put(MFUName, leistungenMFU_NEW);
+
+			// *** write converted extinfo
+			user.setMap(PersistentObject.FLD_EXTINFO, extInfoAsMap);
+		} catch (Exception e1) {
+			// *** if we get here then the MFUList is already converted...
+		}
+	}
+}
